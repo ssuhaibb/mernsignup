bc82abcdf80139d968befad93f18d6b8ca2bf433
suhaib.khan <mohammad.suhaib@mail.vinove.com>
2019-04-15 21:31:12 +0530

    code completed

--- src/actions/authentication.js
+++ src/actions/authentication.js
@@ -1,10 +1,16 @@
 import axios from "axios";
-import { GET_ERRORS } from "./types";
+import { GET_ERRORS, SET_CURRENT_USER } from "./types";
+import setAuthToken from "../setAuthToken";
+import jwt_decode from "jwt-decode";
 
 export const registerUser = (user, history) => dispatch => {
   axios
     .post("/api/users/register", user)
-    .then(res => history.push("login"))
+
+    .then(res => {
+      console.log(res.data);
+      history.push("login");
+    })
     .catch(err => {
       dispatch({
         type: GET_ERRORS,
@@ -14,9 +20,15 @@ export const registerUser = (user, history) => dispatch => {
 };
 
 export const loginUser = user => dispatch => {
+  debugger
   axios
     .post("/api/users/login", user)
     .then(res => {
+      const { token } = res.data;
+      localStorage.setItem("jwtToken", token);
+      setAuthToken(token);
+      const decoded = jwt_decode(token);
+      dispatch(setCurrentUser(decoded));
       console.log(res.data);
     })
     .catch(err => {
@@ -26,3 +38,20 @@ export const loginUser = user => dispatch => {
       });
     });
 };
+
+
+export const setCurrentUser = decoded =>{
+  debugger
+  return{
+    type:SET_CURRENT_USER,
+    payload:decoded
+  }
+}
+
+export const logoutUser = (history) => dispatch => {
+  debugger
+  localStorage.removeItem('jwtToken');
+  setAuthToken(false);
+  dispatch(setCurrentUser({}));
+  history.push('/login');
+}
